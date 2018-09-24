90886e9e971f551dcc9287cab3b66b5c61a8f8ec
rizrizqi <43347792+rizrizqi@users.noreply.github.com>
2018-09-24 17:21:53 +0700

    Add files via upload

---
+++ taskHomeParticipant.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Serialization;
+
+import ObjectSerialization.*;
+import java.io.Serializable;
+
+/**
+ *
+ * @author ASUS
+ */
+public class taskHomeParticipant implements Serializable{
+    private int nim;
+    private String nama;
+    private String asal;
+    private String kelas;
+    
+    public taskHomeParticipant(int nim, String nama, String asal, String kelas){
+        this.nim = nim;
+        this.nama = nama;
+        this.asal = asal;
+        this.kelas = kelas;
+    }
+    public int getNim(){
+        return nim;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public String getAsal(){
+        return asal;
+    }
+    public String getKelas(){
+        return kelas;
+    }
+    public void setNim(int nim){
+        this.nim = nim;
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+    public void setAsal(String asal){
+        this.asal = asal;
+    }
+    
+    public void setKelas(String kelas){
+        this.kelas = kelas;
+    }
+    @Override
+    public String toString(){
+        return nim + " | " + nama + " | " + asal +" | "+ kelas;
+    }
+}
